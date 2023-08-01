3c89b02825a38022e61ab9cde9f326536e1f0d6e
curtis <curtis@serverascode.com>
2023-08-01 15:25:14 -0400

    try using kubeadm config file

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -430,7 +430,7 @@ VERBOSE=false
 UBUNTU_VERSION=22.04
 
 # software versions
-KUBE_VERSION=1.27.3
+KUBE_VERSION=1.27.4
 CONTAINERD_VERSION=1.7.0
 CALICO_VERSION=3.25.0
 CALICO_URL="https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/"
