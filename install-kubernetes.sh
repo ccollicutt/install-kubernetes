98549231c3625d353b6491f16c2ff1fedb94a00e
curtis <curtis@serverascode.com>
2023-08-24 05:23:36 -0400

    bump to 1.28

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -423,7 +423,7 @@ VERBOSE=false
 UBUNTU_VERSION=22.04
 
 # software versions
-KUBE_VERSION=1.27.4
+KUBE_VERSION=1.28.0
 CONTAINERD_VERSION=1.7.0
 CALICO_VERSION=3.25.0
 CALICO_URL="https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/"
